a92997f4a0e9f2c90be200233f429dd7fbdb9600
soyNerin <neri.agustin.es@outlook.com>
2021-02-03 22:38:43 -0300

    Carga de combo de Roles
    Se crea JSON de Roles

--- js/app.js
+++ js/app.js
@@ -28,19 +28,19 @@ function iniciarProvincias(){
 
     const provinces = document.getElementById('provinces');
 
-    var request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
     request.open('GET', 'https://apis.datos.gob.ar/georef/api/provincias', true);    
 
     request.onload = function() {
         if (this.status >= 200 && this.status < 400) {
-            var data = JSON.parse(this.response);
+            const data = JSON.parse(this.response);
 
             data.provincias.sort(function(a,b){
                 return a.id - b.id;
             })
 
             data.provincias.map(function(x){
-                var option = document.createElement("option");
+                const option = document.createElement("option");
                 option.text = x.nombre;
                 option.value = x.id;
 
@@ -59,22 +59,39 @@ function iniciarProvincias(){
     request.send();
 }
 
-function iniciarRoles(){    
-    roles.push(new Rol(1, "Developer"));
-    roles.push(new Rol(2, "SysAdmin"));
-    roles.push(new Rol(3, "DevOps"));
-    roles.push(new Rol(4, "SRE"));
-    roles.push(new Rol(5, "Technical Leader"));
-    roles.push(new Rol(6, "QA"));
-    roles.push(new Rol(7, "Tester"));
-    roles.push(new Rol(8, "Manager"));
-    roles.push(new Rol(9, "Director"));
-    roles.push(new Rol(10, "HelpDesk"));
-    roles.push(new Rol(11, "BI Analyst"));
-    roles.push(new Rol(12, "Data Analyst"));    
-    roles.push(new Rol(13, "Project Manager"));    
-    roles.push(new Rol(14, "Consultant"));    
-    roles.push(new Rol(15, "Architect"));   
+function iniciarRoles(){
+
+    const roles = document.getElementById('roles');
+
+    const request = new XMLHttpRequest();
+    request.open('GET', 'json/roles.json', true);    
+
+    request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            const data = JSON.parse(this.response);
+
+            data.roles.sort(function(a,b){
+                return a.id - b.id;
+            })
+
+            data.roles.map(function(x){
+                const option = document.createElement("option");
+                option.text = x.nombre;
+                option.value = x.id;
+
+                roles.appendChild(option);
+            });
+        } else {
+            // We reached our target server, but it returned an error
+        }
+    };
+
+    request.onerror = function(err) {
+        // There was a connection error of some sort
+        console.error(err);
+    };
+
+    request.send();
 }
   
 function iniciarCuestionario(){
@@ -90,5 +107,5 @@ function iniciarCuestionario(){
 
 // ejecutarLogicaTemporalBienvenida();
 iniciarProvincias();
-// iniciarRoles();
+iniciarRoles();
 // iniciarCuestionario();
