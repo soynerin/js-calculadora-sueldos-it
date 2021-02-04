cf2d3b01f6e4d80a69bd4d5865239966c610ad3c
soyNerin <neri.agustin.es@outlook.com>
2021-02-04 20:57:19 -0300

    Carga de Combo de Bases de Datos, Frameworks, Lenguajes, Plataformas y Roles
    Refactor de funcion que obtiene el JSON por url

--- js/app.js
+++ js/app.js
@@ -1,111 +1,71 @@
-function mostrarAlerta(message){
-    alert(message);
-}
+// function mostrarAlerta(message){
+//     alert(message);
+// }
 
-function mostrarPregunta(message, defaultAnswer){
-    return prompt(message, defaultAnswer);
-}
+// function mostrarPregunta(message, defaultAnswer){
+//     return prompt(message, defaultAnswer);
+// }
 
-function ejecutarLogicaTemporalBienvenida(){
-    if ( confirm("¿Querés probar nuestra página?")) {
-        var name = mostrarPregunta("¿Cuál es tu nombre?", "Invitado");
+// function ejecutarLogicaTemporalBienvenida(){
+//     if ( confirm("¿Querés probar nuestra página?")) {
+//         var name = mostrarPregunta("¿Cuál es tu nombre?", "Invitado");
     
-        while (name !== null && name.length == 0) {
-            mostrarAlerta("¡Por favor, ingresá tu nombre!");
-            name = mostrarPregunta("¿Cuál es tu nombre?", "Invitado");
-        } 
+//         while (name !== null && name.length == 0) {
+//             mostrarAlerta("¡Por favor, ingresá tu nombre!");
+//             name = mostrarPregunta("¿Cuál es tu nombre?", "Invitado");
+//         } 
     
-        mostrarAlerta(`Bienvenido ${name} a nuestra Calculadora de Sueldos`);
-    } else{
-        mostrarAlerta("¡¡Te esperamos la próxima!!");
-    }
-}
+//         mostrarAlerta(`Bienvenido ${name} a nuestra Calculadora de Sueldos`);
+//     } else{
+//         mostrarAlerta("¡¡Te esperamos la próxima!!");
+//     }
+// }
 
-var regiones = new Array();
-var roles = new Array();
+// var regiones = new Array();
+// var roles = new Array();
 
-function iniciarProvincias(){      
+function cargarListaProvincias(){     
+    cargarComboPorJson('provincias', 'https://apis.datos.gob.ar/georef/api/provincias');
+};
 
-    const provinces = document.getElementById('provinces');
+function cargarListaRoles(){
+    cargarComboPorJson('roles', 'json/roles.json', false);
+};
 
-    const request = new XMLHttpRequest();
-    request.open('GET', 'https://apis.datos.gob.ar/georef/api/provincias', true);    
+function cargrListaPataformas(){
+    cargarComboPorJson('plataformas', 'json/plataformas.json', false);
+};
 
-    request.onload = function() {
-        if (this.status >= 200 && this.status < 400) {
-            const data = JSON.parse(this.response);
+function cargarListaLenguajesProgramacion(){
+    cargarComboPorJson('lenguajes', 'json/lenguajes.json', false);
+};
 
-            data.provincias.sort(function(a,b){
-                return a.id - b.id;
-            })
+function cargarListaFrameworks(){
+    cargarComboPorJson('frameworks', 'json/frameworks.json', false);
+};
 
-            data.provincias.map(function(x){
-                const option = document.createElement("option");
-                option.text = x.nombre;
-                option.value = x.id;
-
-                provinces.appendChild(option);
-            });
-        } else {
-            // We reached our target server, but it returned an error
-        }
-    };
-
-    request.onerror = function(err) {
-        // There was a connection error of some sort
-        console.error(err);
-    };
-
-    request.send();
-}
-
-function iniciarRoles(){
-
-    const roles = document.getElementById('roles');
-
-    const request = new XMLHttpRequest();
-    request.open('GET', 'json/roles.json', true);    
-
-    request.onload = function() {
-        if (this.status >= 200 && this.status < 400) {
-            const data = JSON.parse(this.response);
-
-            data.roles.sort(function(a,b){
-                return a.id - b.id;
-            })
-
-            data.roles.map(function(x){
-                const option = document.createElement("option");
-                option.text = x.nombre;
-                option.value = x.id;
-
-                roles.appendChild(option);
-            });
-        } else {
-            // We reached our target server, but it returned an error
-        }
-    };
-
-    request.onerror = function(err) {
-        // There was a connection error of some sort
-        console.error(err);
-    };
-
-    request.send();
+function cargarListaBasesDatos(){
+    cargarComboPorJson('bbdd', 'json/bbdd.json', false);
 }
   
-function iniciarCuestionario(){
-    let participante = new Participante();
+// function iniciarCuestionario(){
+//     let participante = new Participante();
 
-    let region = mostrarPregunta("¿De qué provincia o región sos?", "Buenos Aires");
-    participante.setRegion(region);
+//     let region = mostrarPregunta("¿De qué provincia o región sos?", "Buenos Aires");
+//     participante.setRegion(region);
 
-    let rol = mostrarPregunta("¿Rol a desempeñar?", "Developer");
-    participante.setRol(rol);
+//     let rol = mostrarPregunta("¿Rol a desempeñar?", "Developer");
+//     participante.setRol(rol);
 
-}
+// }
 
 // ejecutarLogicaTemporalBienvenida();
-iniciarProvincias();
-iniciarRoles();
+
+cargarListaProvincias();
+cargarListaRoles();
+cargrListaPataformas();
+cargarListaLenguajesProgramacion();
+cargarListaFrameworks();
+cargarListaBasesDatos();
+
 // iniciarCuestionario();
--- js/functions/base.js
+++ js/functions/base.js
@@ -1,3 +1,43 @@
+const cargarComboPorJson = function cargarComboPorJson(element, url, sortById = true) {
+  const tagElement = document.getElementById(element);
+
+  const request = new XMLHttpRequest();
+  request.open('GET', url, true);    
+
+  request.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+          const data = JSON.parse(this.response);
+
+          if (sortById) {
+            data[element].sort(function(a,b){
+              return a.id - b.id;
+          }) 
+          } else {
+            data[element].sort(function(a,b){
+              return a.nombre > b.nombre;
+          })
+          }
+
+          data[element].map(function(x){
+              const option = document.createElement("option");
+              option.text = x.nombre;
+              option.value = x.id;
+
+              tagElement.appendChild(option);
+          });
+      } else {
+          // We reached our target server, but it returned an error
+      }
+  };
+
+  request.onerror = function(err) {
+      // There was a connection error of some sort
+      console.error(err);
+  };
+
+  request.send();
+}
+
 const getSalarioPorRegion = function getSalario (id) {
     var salario = {
       1: function () {
